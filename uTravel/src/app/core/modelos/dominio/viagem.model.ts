ab3ad793c6e96c3458f33265e7ff6deffaee325d
Laura Beatriz <lauralbbdo18@gmail.com>
2019-11-24 17:12:41 -0200

    adiciona atributos de viagem

--- uTravel/src/app/core/modelos/dominio/viagem.model.ts
+++ uTravel/src/app/core/modelos/dominio/viagem.model.ts
@@ -1,7 +1,20 @@
 import {Entidade, EntidadeBuilder} from './entidade.model';
 
 export class Viagem extends Entidade {
+    titulo: string;
+    objetivo: string;
+    dataInicio: Date;
+    dataFim: Date;
 
+    constructor(titulo: string, objetivo: string, dataInicio: Date, dataFim: Date, id?: number) {
+        super(id);
+
+        this.titulo = titulo;
+        this.objetivo = objetivo;
+
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+    }
 }
 
 export class ViagemBuilder extends EntidadeBuilder<Viagem> {
