bcc29af7bb63043253a9cc72226370f46b640373
Laura Beatriz <lauralbbdo18@gmail.com>
2019-11-24 16:26:47 -0200

    Adiciona atributos de restaurante

--- uTravel/src/app/core/modelos/dominio/restaurante.model.ts
+++ uTravel/src/app/core/modelos/dominio/restaurante.model.ts
@@ -1,9 +1,26 @@
-import {Entidade, EntidadeBuilder} from './entidade.model';
+import { Entidade, EntidadeBuilder } from "./entidade.model";
+import { Endereco } from "./endereco.model";
+import { Empresa } from "./empresa.model";
 
 export class Restaurante extends Entidade {
+    nome: string;
+    numeroEstrelas: number;
+    endereco: Endereco;
+    empresa: Empresa;
 
+    constructor(
+        nome: string,
+        numeroEstrelas: number,
+        id?: number,
+        endereco?: Endereco,
+        empresa?: Empresa
+    ) {
+        super(id);
+        this.nome = nome;
+        this.numeroEstrelas = numeroEstrelas;
+        this.endereco = endereco || null;
+        this.empresa = empresa || null;
+    }
 }
 
-export class RestauranteBuilder extends EntidadeBuilder<Restaurante> {
-
-}
+export class RestauranteBuilder extends EntidadeBuilder<Restaurante> {}
