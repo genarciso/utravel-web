fe8fe1531e5c069a7fa223f814f4bb68d573baa5
Laura Beatriz <lauralbbdo18@gmail.com>
2019-11-24 01:30:30 -0200

    atualiza endpoint da api

--- uTravel/src/environments/environment.prod.ts
+++ uTravel/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     chaveUsuarioAcessoLocalStorage: 'usuario_acesso',
     chaveTokenAcessoLocalStorage: 'token_acesso',
-    apiHost: 'localhost:8080/utravel',
+    apiHost: '179.190.237.238:9000/utravel/swagger',
     apiContext: '/api',
     apiSSL: true
 };
