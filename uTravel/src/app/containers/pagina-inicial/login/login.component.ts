da30385b57e6e89a3f2658a56abc7d9b59b1c92c
Laura Beatriz <lauralbbdo18@gmail.com>
2019-11-24 01:30:47 -0200

    libera o acesso do login para desenvolvimento

--- uTravel/src/app/containers/pagina-inicial/login/login.component.ts
+++ uTravel/src/app/containers/pagina-inicial/login/login.component.ts
@@ -59,11 +59,11 @@ export class LoginComponent implements OnInit {
     }
 
     confirmar() {
-        this.autenticacaoService.entrar(this.login.usuario, this.login.senha);
-        this.autenticacaoService.aoEntrar().subscribe(() => {
-            this.modal.hide();
-            this.router.navigate(['/dashboard']);
-        });
-
+        // this.autenticacaoService.entrar(this.login.usuario, this.login.senha);
+        // this.autenticacaoService.aoEntrar().subscribe(() => {
+        //     this.modal.hide();
+        //     this.router.navigate(['/dashboard']);
+        // });
+        this.router.navigate(['/dashboard']);
     }
 }
