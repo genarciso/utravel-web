61c2235775e00a40251c6444553ba5defa19c892
Laura Beatriz <lauralbbdo18@gmail.com>
2019-11-24 16:49:06 -0200

    Adiciona atributos de hospedagem

--- uTravel/src/app/core/modelos/dominio/hospedagem.model.ts
+++ uTravel/src/app/core/modelos/dominio/hospedagem.model.ts
@@ -1,5 +1,29 @@
-import {Entidade, EntidadeBuilder} from './entidade.model';
+import { Entidade, EntidadeBuilder } from "./entidade.model";
+import { Endereco } from "./endereco.model";
+import { Empresa } from "./empresa.model";
 
-export class Hospedagem extends Entidade {}
+export class Hospedagem extends Entidade {
+    titulo: string;
+    tipoHospedagem: string;
+
+    endereco: Endereco;
+    empresa: Empresa;
+
+    constructor(
+        titulo: string,
+        tipoHospedagem: string,
+        id?: number,
+        endereco?: Endereco,
+        empresa?: Empresa
+    ) {
+        super(id);
+
+        this.titulo = titulo;
+        this.tipoHospedagem = tipoHospedagem;
+
+        this.endereco = endereco || null;
+        this.empresa = empresa || null;
+    }
+}
 
 export class HospedagemBuilder extends EntidadeBuilder<Hospedagem> {}
