0ff26ae704ffd3aa4d352178e96de6ce275753c5
Laura Beatriz <lauralbbdo18@gmail.com>
2019-11-24 17:05:45 -0200

    Adiciona atributos de veículo

--- uTravel/src/app/core/modelos/dominio/veiculo.model.ts
+++ uTravel/src/app/core/modelos/dominio/veiculo.model.ts
@@ -1,9 +1,38 @@
-import {Entidade, EntidadeBuilder} from './entidade.model';
+import { Entidade, EntidadeBuilder } from "./entidade.model";
 
+enum TipoTransporte {
+    BICICLETA,
+    CARRO,
+    MOTO,
+    CAMINHAO,
+    VAN,
+    ONIBUS,
+    AVIAO,
+    HELICOPTERO
+}
 export class Veiculo extends Entidade {
+    placa: string;
+    cor: string;
+    modelo: string;
+    marca: string;
+    tipoTransporte: TipoTransporte;
 
+    constructor(
+        placa: string,
+        cor: string,
+        modelo: string,
+        marca: string,
+        tipoTransporte: TipoTransporte,
+        id?: number
+    ) {
+        super(id);
+
+        this.placa = placa;
+        this.cor = cor;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.tipoTransporte = tipoTransporte;
+    }
 }
 
-export class VeiculoBuilder extends EntidadeBuilder<Veiculo> {
-
-}
+export class VeiculoBuilder extends EntidadeBuilder<Veiculo> {}
